5a41f75cfbd0807fe590de625bd5fe09330adb7a
Ray-Eldath <ray.eldath@gmail.com>
2018-01-25 13:52:05 +0800

    Site updated: 2018-01-25 13:51:58

--- js/src/bootstrap.js
+++ js/src/bootstrap.js
@@ -4,9 +4,12 @@ $(document).ready(function () {
 
   $(document).trigger('bootstrap:before');
 
-  NexT.utils.isMobile() && window.FastClick.attach(document.body);
-
-  NexT.utils.lazyLoadPostsImages();
+    /**
+   * Register JS handlers by condition option.
+   * Need to add config option in Front-End at 'layout/_partials/head.swig' file.
+   */
+  CONFIG.fastclick && NexT.utils.isMobile() && window.FastClick.attach(document.body);
+  CONFIG.lazyload && NexT.utils.lazyLoadPostsImages();
 
   NexT.utils.registerESCKeyEvent();
 
